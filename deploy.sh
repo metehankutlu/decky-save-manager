346c4dfeb46b71d774685ec57bb8f91af45c849b
Metehan Kutlu <metehankutlu@outlook.com>
2022-10-10 11:03:43 +0200

    deploy.sh - set the permissions after deploy

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,34 @@
 
 dir="/home/deck/homebrew/plugins/decky-save-manager"
 
-ssh deck 'rm -r $dir'
+if ssh deck "[ -d $dir ] && rm -r $dir"; then
+    echo "removed the old version"
+fi
 
-ssh deck 'mkdir $dir'
-echo 'created the plugin folder'
+if ssh deck "mkdir $dir"; then
+    echo "created the plugin folder"
+fi
 
-scp -r dist deck:$dir/dist
-echo "copied the dist folder"
-scp main.py deck:$dir/main.py
-echo "copied the main.py file"
-scp game_list.py deck:$dir/game_list.py
-echo "copied the game_list.py file"
-scp plugin.json deck:$dir/plugin.json
-echo "copied the plugin.json file"
-scp package.json deck:$dir/package.json
-echo "copied the package.json file"
-scp README.md deck:$dir/README.md
-echo "copied the README.md file"
+if scp -r dist deck:$dir/dist; then
+    echo "copied the dist folder"
+fi
+
+if scp main.py deck:$dir/main.py; then
+    echo "copied the main.py file"
+fi
+
+if scp game_list.py deck:$dir/game_list.py; then
+    echo "copied the game_list.py file"
+fi
+
+if scp plugin.json deck:$dir/plugin.json; then
+    echo "copied the plugin.json file"
+fi
+
+if scp package.json deck:$dir/package.json; then
+    echo "copied the package.json file"
+fi
+
+if ssh deck "chmod -R 700 $dir"; then
+    echo "permissions are set for the directory"
+fi
